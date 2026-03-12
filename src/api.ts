f7110692297ba86070b1d11a48293f42a0adc4f1
ToxyFlog1627 <toxypochta@gmail.com>
2026-03-12 09:16:36 -0400

    Fix logging key

--- src/api.ts
+++ src/api.ts
@@ -58,5 +58,5 @@ export const persistentLog = async (message: string) => {
     console.log(message);
     const now = new Date();
     const date = `${String(now.getMonth() + 1).padStart(2, "0")}-${String(now.getDate()).padStart(2, "0")} ${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}:${String(now.getSeconds()).padStart(2, "0")}:${String(now.getMilliseconds()).padStart(3, "0")}`;
-    await redis.set(`${now}${Math.floor(Math.random() * 100000)}`, `${date}: ${message}`, { ex: 60 * 60 * 24 });
+    await redis.set(`${Date.now()}${Math.floor(Math.random() * 100000)}`, `${date}: ${message}`, { ex: 60 * 60 * 24 });
 };
